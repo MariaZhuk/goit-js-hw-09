961db46b7abfe819a528d6109d8bfa2a9a5d4881
MariaZhuk <93757214+MariaZhuk@users.noreply.github.com>
2023-07-13 21:40:13 +0000

    Deploying to gh-pages from @ MariaZhuk/goit-js-hw-09@89975bdc447b4de90a2c94bfde4a66c3501c6ba2 🚀

---
+++ 01-color-switcher.a3f3777f.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),bodyColor:document.querySelector("body")};let o=null;t.startBtn.addEventListener("click",(function(){t.startBtn.disabled=!0,t.stopBtn.disabled=!1,o=setInterval((()=>{t.bodyColor.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.stopBtn.disabled=!0,t.startBtn.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.a3f3777f.js.map
